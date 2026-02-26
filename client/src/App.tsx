b24e6bdc5d3ad0a92bd1e44cbe2f9cad3602cee5
Andrewg314 <andrewguo108.dog@gmail.com>
2026-02-26 16:00:36 -0500

    general html structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <section className="bg-[#303030]">
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div>
+          <span>About Us</span>
+          <h1>Connecting Students with Culture</h1>
+          <p>Join, connect, and celebrate Thai traditions with us.</p>
+        </div>
+
+        <div>
+          <div>IMAGE</div>
+          <div>
+            <p>
+              We offer cultural events, social connections, leadership
+              opportunities, and student support.
+            </p>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </section>
   )
 }
 
